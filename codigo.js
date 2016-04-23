af5594353a13077b92b3572e5138ebd7dbc261d7
Yojan Pardo <yojan.pardo@gmail.com>
2016-04-22 19:52:05 -0500

    logica del proyecto con un for pero algo pasa y no sé que es

--- codigo.js
+++ codigo.js
@@ -1,3 +1,13 @@
+var cantidad=prompt("ingresa la cantidad de notas que vas a calcular");
 var nota;
-nota=prompt("ingrese una nota");
-alert("tu nota es " + nota);
+var total=0;
+cantidad=Number(cantidad);
+nota=Number(nota)
+total=parseInt(total);
+for (var i = 0; i < cantidad; i++) {
+	nota=prompt("ingresa la nota numero " + i+1);
+	total=nota+total;
+	alert(total);
+}
+nota=total/cantidad;
+alert("tu nota final es: " + nota);
