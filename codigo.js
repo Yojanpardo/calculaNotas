7614aad5e8a0afbcea50f0f5e2016d87ef050995
Jhonbeltran <jbeltranleon@gmail.com>
2016-04-22 19:18:39 -0500

    Mejoro el calculo de la aplicacion, archivo codigo.js

--- codigo.js
+++ codigo.js
@@ -1,3 +1,8 @@
-var nota;
-nota=prompt("ingrese una nota");
-alert("tu nota es " + nota);
+var nota=prompt("ingrese la primera nota");
+var nota1=prompt("ingrese la segunda nota");
+nota1=Number(nota1);
+nota=Number(nota);
+var total= (nota+nota1)/2
+
+alert("tu nota es " + total);
+
