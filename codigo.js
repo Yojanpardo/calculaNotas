89fc8a1d796ac0870340e0472967b9c300868e7a
Yojan Pardo <yojan.pardo@gmail.com>
2016-04-22 21:38:26 -0500

    reparacion del codigo, ya hace correctamente el promedio, agrego H1 y ya :3

--- codigo.js
+++ codigo.js
@@ -2,12 +2,11 @@ var cantidad=prompt("ingresa la cantidad de notas que vas a calcular");
 var nota;
 var total=0;
 cantidad=Number(cantidad);
-nota=Number(nota)
 total=parseInt(total);
 for (var i = 0; i < cantidad; i++) {
-	nota=prompt("ingresa la nota numero " + i+1);
-	total=nota+total;
-	alert(total);
+	nota=prompt("ingresa la nota numero " + (i+1));
+	nota=Number(nota)
+	total = (nota+total);
 }
 nota=total/cantidad;
 alert("tu nota final es: " + nota);
